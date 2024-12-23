6859ab3af078ef3cc3b769b325b15a94c36d52e8
HongSukju <ehensnfl@gmail.com>
2024-12-23 07:41:39 +0000

    feat: add devcontainer.json

---
+++ .devcontainer/init.sh
@@ -0,0 +1,8 @@
+sudo service postgresql start
+
+DB_NAME=kwitch
+DB_EXIST=$(psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'")
+
+if [ "$DB_EXIST" != "1" ]; then
+    psql -U postgres -c "CREATE DATABASE kwitch;"
+fi
