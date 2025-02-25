d07224324ca19e325fddba1c52224aa2809ec7ef
Sukju Hong <ehensnfl@gmail.com>
2025-02-25 04:13:47 +0000

    feat: add video controller

--- apps/web/app/channels/[channelId]/page.tsx
+++ apps/web/app/channels/[channelId]/page.tsx
@@ -3,7 +3,14 @@
 import { useLayoutEffect, useState, useRef } from "react"
 
 import { useToast } from "@kwitch/ui/hooks/use-toast"
-import { SignalSlashIcon } from "@heroicons/react/24/solid"
+import {
+  ArrowsPointingOutIcon,
+  PlayIcon,
+  SignalSlashIcon,
+  SpeakerWaveIcon,
+  SpeakerXMarkIcon,
+  StopIcon,
+} from "@heroicons/react/24/solid"
 import { useParams } from "next/navigation"
 import * as mediasoup from "mediasoup-client"
 import { Streaming } from "@kwitch/types"
@@ -13,6 +20,7 @@ import { useSocket } from "@/provider/socket-provider"
 import { createConsumer, createDevice, createTransport } from "@/lib/mediasoup"
 import { ChatComponent } from "@/components/channels/chat"
 import { useAuth } from "@/provider/auth-provider"
+import { Slider } from "@kwitch/ui/components/slider"
 
 export default function ChannelPage() {
   const params = useParams<{ channelId: string }>()
@@ -24,13 +32,51 @@ export default function ChannelPage() {
 
   const [onAir, setOnAir] = useState<boolean>(false)
   const [streaming, setStreaming] = useState<Streaming | null>(null)
+  const [isPlaying, setIsPlaying] = useState<boolean>(true)
+  const [isMuted, setIsMuted] = useState<boolean>(true)
+  const [volume, setVolume] = useState<number>(100)
 
   const videoRef = useRef<HTMLVideoElement | null>(null)
+  const videoControllerRef = useRef<HTMLDivElement | null>(null)
 
   const getProducer = async () => {
     return new Promise<string[]>((resolve) => {
       socket!.emit(SOCKET_EVENTS.MEDIASOUP_GETALL_PRODUCER, channelId, resolve)
     })
+  }
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause()
+      } else {
+        videoRef.current.play()
+      }
+      setIsPlaying(!isPlaying)
+    }
+  }
+
+  const handleMuteUnmute = () => {
+    if (videoRef.current) {
+      videoRef.current.muted = !isMuted
+      setIsMuted(!isMuted)
+    }
+  }
+
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newVolume = parseFloat(event.target.value)
+    if (videoRef.current) {
+      videoRef.current.volume = newVolume
+    }
+    setVolume(newVolume)
+  }
+
+  const handleFullscreen = () => {
+    if (videoRef.current) {
+      videoRef.current.requestFullscreen().catch((error) => {
+        console.error("Error requesting fullscreen:", error)
+      })
+    }
   }
 
   useLayoutEffect(() => {
@@ -108,12 +154,52 @@ export default function ChannelPage() {
   return onAir ? (
     <>
       <div className='w-full flex flex-col overflow-y-auto scrollbar-hidden'>
-        <video
-          className='w-full mx-auto aspect-video'
-          ref={videoRef}
-          autoPlay
-          muted
-        />
+        <div
+          className='relative'
+          onMouseEnter={() =>
+            videoControllerRef.current?.classList.remove("opacity-0")
+          }
+          onMouseLeave={() =>
+            videoControllerRef.current?.classList.add("opacity-0")
+          }
+        >
+          <video
+            className='w-full mx-auto aspect-video'
+            ref={videoRef}
+            autoPlay
+            muted={isMuted}
+          />
+          <div
+            ref={videoControllerRef}
+            className='absolute opacity-0 transition-opacity duration-300 bottom-0 left-0 right-0 p-2 flex items-center gap-x-3'
+          >
+            <button onClick={handlePlayPause}>
+              {isPlaying ? (
+                <StopIcon className='size-4' />
+              ) : (
+                <PlayIcon className='size-4' />
+              )}
+            </button>
+            <button onClick={handleMuteUnmute}>
+              {isMuted ? (
+                <SpeakerWaveIcon className='size-4' />
+              ) : (
+                <SpeakerXMarkIcon className='size-4' />
+              )}
+            </button>
+            <Slider
+              defaultValue={[volume]}
+              max={100}
+              step={1}
+              className='w-24'
+              onChange={handleVolumeChange}
+            />
+            <div className='flex-1'></div>
+            <button onClick={handleFullscreen}>
+              <ArrowsPointingOutIcon className='size-4' />
+            </button>
+          </div>
+        </div>
         {streaming && <StreamingInfo streaming={streaming} />}
       </div>
       <div className='hidden xl:block'>
