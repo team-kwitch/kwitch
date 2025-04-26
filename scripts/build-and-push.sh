3d2d0dc79ae4a173bd3e381f85dd0b14b7692784
Sukju Hong <ehensnfl@gmail.com>
2025-04-26 23:23:42 +0900

    feat: add dockerfile for api application

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -7,8 +7,11 @@ TAG=${1:-latest}  # 첫 번째 인자로 태그를 받으며, 없으면 기본�
 # 이미지 정보
 IMAGES=(
   "sukjuhong/kwitch-api:apps/api/Dockerfile"
-  "sukjuhong/kwitch-web:apps/web/Dockerfile"
 )
+
+# docker login
+echo "🔑 Logging in to Docker Hub..."
+docker login -u sukjuhong
 
 # 스크립트 시작
 echo "🚀 Starting build and push process..."
@@ -16,11 +19,11 @@ echo "🔖 Using tag: $TAG"
 
 # 각 이미지 빌드 및 푸시
 for IMAGE_INFO in "${IMAGES[@]}"; do
-  IMAGE_NAME=$(echo $IMAGE_INFO | cut -d':' -f1)
-  DOCKERFILE_PATH=$(echo $IMAGE_INFO | cut -d':' -f2)
+  IMAGE_NAME=$(echo "$IMAGE_INFO" | cut -d':' -f1)
+  DOCKERFILE_PATH=$(echo "$IMAGE_INFO" | cut -d':' -f2)
 
   echo "🏗️ Building Docker image: $IMAGE_NAME:$TAG"
-  docker build -t $IMAGE_NAME:$TAG -f $DOCKERFILE_PATH .
+  docker build -t "$IMAGE_NAME:$TAG" -t "$IMAGE_NAME:latest" -f "$DOCKERFILE_PATH" .
   if [ $? -ne 0 ]; then
     echo "❌ Docker build failed for $IMAGE_NAME!"
     exit 1
@@ -28,9 +31,9 @@ for IMAGE_INFO in "${IMAGES[@]}"; do
   echo "✅ Docker image built successfully: $IMAGE_NAME:$TAG"
 
   echo "📤 Pushing Docker image: $IMAGE_NAME:$TAG"
-  docker push $IMAGE_NAME:$TAG
+  docker push "$IMAGE_NAME:$TAG"
   if [ $? -ne 0 ]; then
-    echo "❌ Docker push failed for $IMAGE_NAME!"
+    echo "❌ Docker push failed for $IMAGE_NAME"
     exit 1
   fi
   echo "✅ Pushed successfully: $IMAGE_NAME:$TAG"
